917f7dc57e6f9eae5efc5cbea53ae44e50af08b8
Lucas Mercier <157757297+lumercier@users.noreply.github.com>
2025-04-01 21:28:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ a = 150
 b = 3
 c = a + b
 
-y = "Paris"
-
 name = "Lucas"
 surname = "Mercier"
 
@@ -11,3 +9,25 @@ full_name = name + " " + surname
 
 print(c)
 print(full_name)
+
+#Task
+#1. Write if else statement to validate if x is larger than y. Return true if yes
+
+x = 10
+y = 9
+
+def compare (x,y):
+  if x>y :
+    return True
+  return False
+
+print(compare(10,9))
+
+#2. Write if else statement to validate if x is larger than y and less than b Return true if yes
+
+def compare2 (x,y,b):
+  if (x>y) and (x<b):
+    return True
+  return False
+
+print(compare2(10,9,300)
